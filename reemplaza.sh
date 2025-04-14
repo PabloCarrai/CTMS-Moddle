da7d19c37d57479b73d5aaf196063240e81a2869
PabloCarrai <pablohernanjuan@hotmail.com>
2025-04-14 14:58:57 -0300

    Cambiando algo que estaba mal

--- reemplaza.sh
+++ reemplaza.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-var1='post_max_size = 4M'
-var2='post_max_size = 1000M'
-var3='upload_max_filesize = 4M'
-var4='upload_max_filesize = 1000M'
+var1='post_max_size = 40M'
+var2='post_max_size = 2000M'
+var3='upload_max_filesize = 40M'
+var4='upload_max_filesize = 2000M'
 sed -i "s&$var1&$var2&g" /opt/bitnami/php/etc/php.ini 
 sed -i "s&$var3&$var4&g" /opt/bitnami/php/etc/php.ini 
 /opt/bitnami/scripts/apache/reload.sh
